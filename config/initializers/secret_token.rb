c499d99ef1b70f7a3855ef33a25a77b25088133b
Alan Miles <alanpqs@gmail.com>
2011-08-07 00:26:15 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jobscape::Application.config.secret_token = '7a4c1b0df5e807391f9e41c2ed0dbac15c1a857ad4dbfaae9e4edf2eb0fbf9903408fee53c59bb0776d2ee3b1dcee541344749d3b6a329d9a52906d14ad70420'
