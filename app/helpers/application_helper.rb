63d4b659e42a5a4fdcb1546de06642e93f6b54ca
Alan Miles <alanpqs@gmail.com>
2011-08-13 13:32:08 +0100

    Tidy up

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
 
+  def logo
+    logo = image_tag("logo.png", :alt => "JobScape", :class => "round")
+  end
+  
   # Return a title on a per-page basis.
   def title
     base_title = "JobScape"
