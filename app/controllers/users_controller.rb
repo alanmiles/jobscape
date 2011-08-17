e4632207a4ec5c21b7afec9fb2d334fd6ed4fc69
Alan Miles <alanpqs@gmail.com>
2011-08-17 12:27:57 +0100

    Done with user edit/update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
+  before_filter :authenticate, :only => [:index, :edit, :update, :destroy]
+  before_filter :correct_user, :only => [:edit, :update]
+  before_filter :admin_user,   :only => :destroy
+  before_filter :signedin,     :only => [:new, :create]
+  
+   def index
+    @title = "All users"
+    @users = User.paginate(:page => params[:page])
+  end
   
   def show
     @user = User.find(params[:id])
@@ -23,4 +32,49 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  
+  def edit
+    @title = "Edit user"
+  end
+  
+  def update
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated."
+      redirect_to @user
+    else
+      @title = "Edit user"
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @user = User.find(params[:id])
+    if @user == current_user
+      flash[:notice] = "You're not allowed to delete your own record."
+    else
+      @user.destroy
+      flash[:success] = "User destroyed."
+    end
+    redirect_to users_path
+  end
+  
+  private
+
+    def authenticate
+      deny_access unless signed_in?
+    end
+    
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
+    
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
+    end
+    
+    def signedin
+      flash[:notice] = "You're already signed up"
+      redirect_to(user_path(current_user)) if signed_in? 
+    end
 end
