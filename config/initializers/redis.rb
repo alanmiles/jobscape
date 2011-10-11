eba593ede1daa3069bde0c797a19d526e305c507
Alan Miles <alanpqs@gmail.com>
2011-10-11 04:18:54 +0100

    Revised config files redis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,5 @@
 #$redis = Redis.new(:host => 'localhost', :port => 6379)
-REDIS = Redis.connect(:url => ENV["REDISTOGO_URL"])
+#REDIS = Redis.connect(:url => ENV["REDISTOGO_URL"])
+uri = URI.parse(ENV["REDISTOGO_URL"])
+REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 
