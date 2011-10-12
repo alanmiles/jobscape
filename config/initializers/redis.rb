c1fb800fb42d693007fab9f64f736407f5a35cc8
Alan Miles <alanpqs@gmail.com>
2011-10-12 13:40:41 +0100

    clean-up

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,12 @@
-#$redis = Redis.new(:host => 'localhost', :port => 6379)
-#REDIS = Redis.connect(:url => ENV["REDISTOGO_URL"])
-uri = URI.parse(ENV["REDISTOGO_URL"])
-REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+if Rails.env.development? or Rails.env.test?
+  @path = 'redis://localhost:6379'
+else 
+  @path = ENV["REDISTOGO_URL"]
+end 
+  
 
+##uri = URI.parse(ENV["REDISTOGO_URL"])
+uri = URI.parse(@path)
+REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+##$redis = Redis.new(:host => 'localhost', :port => 6379)
+##REDIS = Redis.connect(:url => ENV["REDISTOGO_URL"])
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,7 +8,6 @@ Spork.prefork do
   ENV["RAILS_ENV"] ||= 'test'
   require File.expand_path("../../config/environment", __FILE__)
   require 'rspec/rails'
-
   # Requires supporting files with custom matchers and macros, etc,
   # in ./support/ and its subdirectories.
   Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
